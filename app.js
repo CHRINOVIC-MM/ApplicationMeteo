e2ec07a5990494847fda49f45ef130ea55f1eea2
= <=>
2023-02-23 19:27:20 +0100

    Ajout de fonction pour mettre à jour la date

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ let currentUnit = '';
 let hourlyOrWeek = 'week';
 
 //Update date time
+
+function getDateTime(){
+    let now = new Date(),
+        hour = now.getHours(),
+        minute = now.getMinutes();
+
+    let days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+
+    hour = hour % 12;
+    if (hour  < 10) {
+        hour = '0' + hour;
+    }
+    if (minute < 10) {
+        minute = '0' + minute;
+    }
+
+    let dayString = days[now.getDay()];
+
+    return `${dayString}, ${hour}:${minute}`
+}
